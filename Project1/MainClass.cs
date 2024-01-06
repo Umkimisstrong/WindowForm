de064cbe97cf0104e8ab36163ba9261610d39ebb
KIM SANG KI <96037218+Umkimisstrong@users.noreply.github.com>
2024-01-06 16:08:43 +0900

    Test Class is Created
    Test Class is Created

---
+++ Project1/MainClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class MainClass
+    {
+        public static void Main(string []  args)
+        {
+            TestClass01 obj = new TestClass01("Hi");
+
+            string msg = obj.getMessage(string.Empty);
+
+            string msg1 = obj.getMessage("Hello");
+
+            Console.WriteLine(msg);
+            Console.WriteLine(msg1);
+
+        }
+
+    }
+}
